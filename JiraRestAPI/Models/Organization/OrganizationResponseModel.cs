1c7e63847adfdad63a7dbf57db06b4643dc2934d
ademv <36985334+ademv@users.noreply.github.com>
2019-01-22 16:13:42 +0100

    new commit

---
+++ JiraRestAPI/Models/Organization/OrganizationResponseModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JiraRestAPI.Models.Organization
+{
+    public class OrganizationResponseModel
+    {
+        public OrganizationResponseModel()
+        {
+            Messages = new List<BaseResponseMessage>();
+    }
+
+        public string UserKey { get; set; }
+        public  string DisplayName { get; set; }
+        public bool Status { get; set; }
+        public List<BaseResponseMessage> Messages { get; set; }
+
+        public void CalculateStatus()
+        {
+
+            var haserror = Messages.Where(y => y.status == false).Any();
+
+            Status = !haserror;
+        }
+    }
+}
---
+++ JiraRestAPI/Models/TransactionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JiraRestAPI.Models
+{
+    public class TransactionModel
+    {
+
+        public bool Status { get; set; }
+
+        public object Data { get; set; }
+    }
+}
